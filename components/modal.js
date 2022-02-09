f53361126e40169a870cabe16c866085f2598f57
shivanigangadharan <shivani.gangadharan@gmail.com>
2022-02-09 13:10:50 +0530

    modal bug fixed

--- components/modal.js
+++ components/modal.js
@@ -1,16 +1,14 @@
-const modal = document.getElementById("modal");
+const modal = document.getElementById("modalBox");
 const action1 = document.getElementById("action1");
 const action2 = document.getElementById("action2");
 const openBtn = document.getElementById("openmodal");
 
 function close() {
-    console.log("close clicked");
     modal.setAttribute("style", "display:none");
 }
 
 function open() {
-    console.log("open clicked");
-    modal.setAttribute("style", "display:block");
+    modal.setAttribute("style", "display:block")
 }
 
 action1.addEventListener("click", close);
